af485215f0968f3187396783c12b642cc6a791d1
Anna Cox <annalisecox@gmail.com>
2022-05-18 13:34:28 -0700

    Add basic styling for Search screen

---
+++ src/screen/Search/Search.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {SafeAreaView, View, TextInput, StyleSheet} from 'react-native';
+
+import Text from 'component/base/Text';
+
+import mockData from './searchMockData';
+
+const Search: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Where are you going?"
+          placeholderTextColor="#858585"
+          textAlign="center"
+        />
+        <View style={styles.list}>
+          {mockData.cities.map(city => (
+            <View style={styles.listItem}>
+              <Text variant="bodyLarge">{city}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Search;
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.19)',
+    borderRadius: 100,
+    height: 60,
+    marginTop: 16,
+    marginBottom: 32,
+    marginHorizontal: 24,
+    fontFamily: 'EncodeSans-Regular',
+    fontSize: 16,
+    lineHeight: 20,
+  },
+  list: {
+    marginHorizontal: 72,
+  },
+  listItem: {
+    marginBottom: 10,
+  },
+});
