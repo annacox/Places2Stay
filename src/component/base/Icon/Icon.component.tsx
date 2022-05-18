56011b57a9dc877b8d1eca09c2890994aab7db33
Anna Cox <annalisecox@gmail.com>
2022-05-18 15:51:44 -0700

    Add search icon

--- src/component/base/Icon/Icon.component.tsx
+++ src/component/base/Icon/Icon.component.tsx
@@ -1,5 +1,11 @@
 import {Place} from './svg';
 import {Calendar} from './svg';
 import {Home} from './svg';
+import {Search} from './svg';
 
-export default {place: Place, calendar: Calendar, home: Home} as const;
+export default {
+  place: Place,
+  calendar: Calendar,
+  home: Home,
+  search: Search,
+} as const;
--- src/component/base/Icon/svg/index.tsx
+++ src/component/base/Icon/svg/index.tsx
@@ -1,3 +1,4 @@
 export {default as Place} from './Place';
 export {default as Calendar} from './Calendar';
 export {default as Home} from './Home';
+export {default as Search} from './Search';
