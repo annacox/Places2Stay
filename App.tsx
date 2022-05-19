158d189973096f99572b53b2b68365b81c294776
Anna Cox <annalisecox@gmail.com>
2022-05-19 11:25:54 -0700

    Add bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -9,19 +9,43 @@
  */
 
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from 'screen/Home';
 import Stay from 'screen/Stay';
 import Search from 'screen/Search';
+import Icon from 'component/base/Icon';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      {/* <Home /> */}
-      {/* <Stay /> */}
-      <Search />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Stay') {
+              iconName = 'calendar';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            }
+
+            return <Icon name={iconName} size="large" color={color} />;
+          },
+          tabBarActiveTintColor: '#000000',
+          tabBarInactiveTintColor: '#858585',
+          tabBarShowLabel: false,
+          headerShown: false,
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Stay" component={Stay} />
+        <Tab.Screen name="Search" component={Search} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
